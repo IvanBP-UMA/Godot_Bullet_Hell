e2b675c3aecddb2832592bb0055119638c72aa8f
ivanchu_12 <ivanbezaresp@gmail.com>
2024-04-27 11:45:00 +0200

    Working sine bullets

--- scripts/nodes_scripts/math_bullet.gd
+++ scripts/nodes_scripts/math_bullet.gd
@@ -2,11 +2,17 @@ extends Bullet
 class_name MathBullet
 
 enum mathFunctions {sine, cosine, tangent, squareRoot, square, cubic, logaritmic}
+var originalDirection: Vector2
 var function: mathFunctions
 var independentVar: float = 0
 var step: float = 0.1
+var setUp: bool = false
 
 func _process(delta):
+	if (!setUp):
+		originalDirection = direction
+		print(originalDirection)
+		setUp = true
 	super(delta)
 	updateDirection()
 	
@@ -16,17 +22,20 @@ func mathBulletSetUp(function: mathFunctions, step: float):
 
 func updateDirection():
 	var nextPosition: Vector2
+	var currentPosition: Vector2
+	currentPosition.x = independentVar
 	independentVar += step
 	nextPosition.x = independentVar
 	var currentDirectionAngle = direction.angle()
 	
 	match function:
 		mathFunctions.sine:
+			currentPosition.y = sin(independentVar-step)
 			nextPosition.y = sin(independentVar)
 		mathFunctions.cosine:
 			nextPosition.y = cos(independentVar)
 		mathFunctions.square:
 			nextPosition.y = pow(independentVar, 2)
 			
-	direction = direction.rotated(currentDirectionAngle - nextPosition.rotated(currentDirectionAngle).angle())
+	direction = (nextPosition-currentPosition).rotated(originalDirection.angle()).normalized()
 	
